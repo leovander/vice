18f969c3244ae4c9eaccb1d0831dfa44baedd0ae
Israel Torres <itorres1490@gmail.com>
2015-05-11 10:07:13 -0700

    Updated to System Fonts, looks a lot cleaner. Also, solid color background to take advantage of color screen.

--- src/main.c
+++ src/main.c
@@ -1,12 +1,9 @@
 #include <pebble.h>
-  
+
 static Window *s_main_window;
 static TextLayer *main_time_layer;
 static TextLayer *shadow_time_layer;
 static TextLayer *date_layer;
-static TextLayer *time_back_layer;
-static GFont s_custom_font_40;
-static GFont s_custom_font_30;
 
 //Colors
 /*
@@ -26,7 +23,7 @@ char *upcase(char *str)
 
 static void update_time() {
   // Get a tm structure
-  time_t temp = time(NULL); 
+  time_t temp = time(NULL);
   struct tm *tick_time = localtime(&temp);
 
   // Create a long-lived buffer
@@ -41,10 +38,10 @@ static void update_time() {
     //Use 12 hour format
     strftime(buffer, sizeof(buffer), "%I %M", tick_time);
   }
-  
+
   strftime(date_buffer, sizeof(date_buffer), "%d %b", tick_time);
   upcase(date_buffer);
-  
+
   // Display this time on the TextLayer
   text_layer_set_text(main_time_layer, buffer);
   text_layer_set_text(shadow_time_layer, buffer);
@@ -53,66 +50,55 @@ static void update_time() {
 
 static void main_window_load(Window *window) {
   // Create time TextLayer
-  main_time_layer = text_layer_create(GRect(0, 75, 138, 50));
+  main_time_layer = text_layer_create(GRect(0, 77, 138, 56));
   text_layer_set_background_color(main_time_layer, GColorClear);
   text_layer_set_text_color(main_time_layer, GColorShockingPink);
   text_layer_set_text(main_time_layer, "00 00");
 
-  text_layer_set_font(main_time_layer, s_custom_font_40);
+  text_layer_set_font(main_time_layer, fonts_get_system_font(FONT_KEY_LECO_38_BOLD_NUMBERS));
   text_layer_set_text_alignment(main_time_layer, GTextAlignmentCenter);
-  
+
   // Create shadow TextLayer
-  shadow_time_layer = text_layer_create(GRect(5, 80, 138, 50));
+  shadow_time_layer = text_layer_create(GRect(5, 82, 138, 56));
   text_layer_set_background_color(shadow_time_layer, GColorClear);
   text_layer_set_text_color(shadow_time_layer, GColorBlack);
   text_layer_set_text(shadow_time_layer, "00 00");
 
-  text_layer_set_font(shadow_time_layer, s_custom_font_40);
+  text_layer_set_font(shadow_time_layer, fonts_get_system_font(FONT_KEY_LECO_38_BOLD_NUMBERS));
   text_layer_set_text_alignment(shadow_time_layer, GTextAlignmentCenter);
-  
+
   // Create date TextLayer
   date_layer = text_layer_create(GRect(0, 40, 144, 50));
   text_layer_set_background_color(date_layer, GColorClear);
-  text_layer_set_text_color(date_layer, GColorCyan);
+  text_layer_set_text_color(date_layer, GColorWhite);
   text_layer_set_text(date_layer, "01 Jan");
 
-  text_layer_set_font(date_layer, s_custom_font_30);
+  text_layer_set_font(date_layer, fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK));
   text_layer_set_text_alignment(date_layer, GTextAlignmentCenter);
-  
-  // Create time background TextLayer
-  time_back_layer = text_layer_create(GRect(0, 80, 144, 35));
-  text_layer_set_background_color(time_back_layer, GColorCyan);
 
   // Add it as a children layer to the Window's root layer
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(time_back_layer));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(shadow_time_layer));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(main_time_layer));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(date_layer));
-  
+
   // Make sure the time is displayed from the start
   update_time();
 }
 
 static void main_window_unload(Window *window) {
-  // Destroy TextLayer
-  fonts_unload_custom_font(s_custom_font_40);
-  fonts_unload_custom_font(s_custom_font_30);
   text_layer_destroy(main_time_layer);
   text_layer_destroy(shadow_time_layer);
   text_layer_destroy(date_layer);
-  text_layer_destroy(time_back_layer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
 }
-  
+
 static void init() {
   // Create main Window element and assign to pointer
   s_main_window = window_create();
-  window_set_background_color(s_main_window, GColorWhite);
-  s_custom_font_40 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_BROADWAY_40));
-  s_custom_font_30 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_BROADWAY_30));
+  window_set_background_color(s_main_window, GColorCyan);
 
   // Set handlers to manage the elements inside the Window
   window_set_window_handlers(s_main_window, (WindowHandlers) {
@@ -122,7 +108,7 @@ static void init() {
 
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
-  
+
   // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
